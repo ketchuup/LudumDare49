8b0046cadc57fc6ae81c84b00eb60bbe0d5876e4
ketchuup <antonYmanin@gmail.com>
2021-10-02 17:15:05 +0300

    Add player movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,36 +1,56 @@
+using System;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 40f;
-    public MovementController controller;
+    public float maxSpeed = 10f;
+    public float jumpHeight = 10f;
+    public float slowingDownCoefficient = 1f;
+    
+    private Rigidbody2D rigidbody;
+    private float horizontalInput;
+    private bool isGrounded = true;
 
-    private float horizontalMovement;
-    private bool isJumping;
-    private bool isCrouching;
-
-    void Update()
+    private void Start()
     {
-        horizontalMovement = Input.GetAxisRaw("Horizontal") * speed;
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
 
-        if (Input.GetButtonDown("Jump"))
-        {
-            isJumping = true;
-        }
+    private void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        rigidbody.AddForce(new Vector2(horizontalInput, 0), ForceMode2D.Impulse);
 
-        if (Input.GetButtonDown("Crouch"))
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            isCrouching = true;
+            rigidbody.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+            isGrounded = false;
         }
-        else if (Input.GetButtonUp("Crouch"))
+        
+        if (Input.GetButtonDown("Slow"))
         {
-            isCrouching = false;
+            rigidbody.AddForce(new Vector2(0, -slowingDownCoefficient * (3 * Convert.ToInt16(!isGrounded))), ForceMode2D.Impulse);
+        }
+        
+        if (Mathf.Abs(rigidbody.velocity.x) > maxSpeed)
+        {
+            rigidbody.velocity = new Vector2(maxSpeed * Mathf.Sign(rigidbody.velocity.x), rigidbody.velocity.y);
         }
     }
 
-    void FixedUpdate()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        controller.Move(horizontalMovement * Time.fixedDeltaTime, isCrouching, isJumping);
-        isJumping = false;
+        if (collision.gameObject.CompareTag("Ground") && !isGrounded)
+        {
+            isGrounded = true;
+        }
+    }
+    
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // if (collision.gameObject.CompareTag("Ground"))
+        // {
+        //     isGrounded = false;
+        // }
     }
 }
